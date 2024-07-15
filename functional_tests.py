d4a11f68c9d3c7aef01ba7d9357c9dbc2378c491
Ruben Sa <rubenjms1@gmail.com>
2024-07-15 20:55:14 -0300

    Primeiro FT especificado nos comentários, utilizando agora o unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,41 @@
 from selenium import webdriver
+import unittest
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+    def tearDown(self):
+        self.browser.quit()
+        #Edith ouviu falar de uma nova aplicação online interessante para
+        #lista de tarefas. Ela decide verificar sua homepage
+    def test_can_start_a_list_and_retrive_it_later(self):
+        self.browser.get('http://localhost:8000')
 
-browser = webdriver.Firefox()
+        #Ela percebe que o título da página e o cabeçalho mencionam listas
+        #tarefas (to-do)
 
-browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('finish the test!')
 
-assert 'The install worked successfully! Congratulations!' in browser.title
+        #Ela é convidada a inserir um item de tarefa imediatamente 
+
+        #Ela digita "Buy peacock feathers" (comprar penas de pavão) em uma caixa
+        #de texto (o hobby de Edith é fazer iscas para pesca com fly)   
+
+        #Quando ela tecla enter, a página é atualizada, e agora a página lista
+        #"1: buy peacock feathers" como um item em uma lista de tarefas
+
+        #Ainda continua havendo uma caixa de texto convidando-a a acrescentar outro
+        #item. Ela insere "Use peacock feathers to make a fly" (Usar penas de pavão
+        #para fazer um fly - Edith é bem metódica)
+
+        #A pagina é atualizada novamente e agora mostra os dois itens em sua lista
+
+        # Edith se pergunta se o site lembrará de sua lista. Então ela nota
+        #que o site gerou um url único para ela -- há um pequeno
+        #texto explicativo para isso.
+
+        # Ela acessa esse URL - sua lista de tarefas continua lá
+
+        #Satisfeita, ela volta a dormir
+if __name__ == '__main__':
+    unittest.main()
