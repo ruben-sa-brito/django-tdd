71c3653590d4e92a9e3831e5d53f52355800a026
Ruben Sa <rubenjms1@gmail.com>
2024-07-17 09:32:09 -0300

    Model for list Items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,30 @@
 from django.urls import resolve
 from django.test import TestCase
+from lists.models import Item
 
 from lists.views import home_page
 
 class HomePageTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+
+        self.assertAlmostEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertAlmostEqual(second_saved_item.text, 'Item the second')
+
+        
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
